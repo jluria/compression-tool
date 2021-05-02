0fc2c0b9a3e8840a6df476959890db7bdb0134ed
Josh Luria <josh.luria@gmail.com>
2021-05-02 11:12:07 -0400

    the program should open gui window

---
+++ ui/Gui.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+class Gui
+{
+  public static void main(String args[])
+  {
+    JFrame mainWindow = new JFrame("Zipbox");
+    mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    mainWindow.setSize(300,300);
+
+    mainWindow.setVisible(true);
+  }
+}
